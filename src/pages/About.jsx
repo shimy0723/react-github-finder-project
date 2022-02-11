195f159df2346675b702803b5f7d62934977ad79
shimy0723 <shimy0723@gmail.com>
2022-02-12 01:46:45 +1300

    latest update; Pages updated;

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,24 @@
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1 className='text-6xl mb-4'>Github Finder Project</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details. This
+        project was learnt from the
+        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
+          {' '}
+          React Front To Back
+        </a>{' '}
+        Udemy course by
+        <strong>
+          <a href='https://traversymedia.com'> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+    </div>
   )
 }
 
