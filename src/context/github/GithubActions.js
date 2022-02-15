40beed60766c384244a1ed983197c3cd870efb0c
shimy0723 <shimy0723@gmail.com>
2022-02-16 09:10:31 +1300

    axios installed; fetch updated;

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,12 @@
+import axios from 'axios'
+
 const BASE_URL = process.env.REACT_APP_GITHUB_BASE_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+const github = axios.create({
+  baseURL: BASE_URL,
+  headers: { Authorization: `token ${GITHUB_TOKEN}` }
+})
 
 // Search users
 export const searchUsers = async(text) => {
@@ -7,49 +14,17 @@ export const searchUsers = async(text) => {
     q: text,
     per_page: 100,
   })
-
-  const response = await fetch(`${BASE_URL}/search/users?${params}`,
-  {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  })
   
-  const {items} = await response.json()
-  return items
+  const response = await github.get(`/search/users?${params}`)
+  return response.data.items
 }
 
-// Get a single user
-export const getUser = async(login) => {
-  const response = await fetch(`${BASE_URL}/users/${login}`,
-  {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  })
+// Get user and repos
+export const getUserAndRepos = async(login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`)
+  ])
 
-  if(response.status === 404){
-    window.location = '/404'
-  } else {
-    const data = await response.json()
-    return data
-  }    
-}
-
-  // Get user repos
-export const getUserRepos = async(login) => {
-  const params = new URLSearchParams({
-    sort: 'created',
-    per_page: 10,
-  })
-
-  const response = await fetch(`${BASE_URL}/users/${login}/repos?${params}`,
-  {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  })
-  
-  const data = await response.json()
-  return data
+  return { user: user.data, repos: repos.data }
 }
